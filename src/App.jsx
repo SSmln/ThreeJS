29594ca773829b908a0977ad34b074f6d341459d
ssmln <lsm7630@gmail.com>
2024-01-07 01:13:43 +0900

    threejs 1

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    // tailwind css usage
+    <div className={" bg-indigo-800 absolute w-full h-screen p-0 top-0 left-0"}>
+      <div className={"h-5/6 p-0 "}>
+        <Canvas
+          camera={{
+            position: [0, 0, 7],
+            fov: 30,
+          }}
+        >
+          <color attach="background" args={["#ececec"]} />
+          // three-drei usage
+          <OrbitControls />
+          <mesh rotation={[Math.PI / 10, 10, 10]}>
+            // three-fiber usage
+            <torusGeometry />
+            <meshNormalMaterial />
+          </mesh>
+        </Canvas>
+        <div className={"text-current text-7xl italic font-light z-40 p-5"}>
+          yaee! you made it{" "}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
